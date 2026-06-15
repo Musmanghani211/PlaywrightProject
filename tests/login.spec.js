37bcc993c3f98d835efc432ac896ae191627a616
Musmanghani211 <musmanghani0211@gmail.com>
2026-06-15 15:32:52 +0500

    again added the same code after revert

---
+++ tests/login.spec.js
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test';
+
+test ('verify the user is able to login with valid crededentials', async () => {
+
+await page.goto (' await page.goto(https://playwright.dev/');
+await expect(page).toHaveTitle('/Playwright/');
+
+})
