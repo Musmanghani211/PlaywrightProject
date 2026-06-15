ab0469bdab0bfe2441b8d83eee497885e57fb064
Musmanghani211 <musmanghani0211@gmail.com>
2026-06-15 15:58:39 +0500

    added 1 new test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,17 @@
 import {test, expect} from '@playwright/test';
 
-test ('verify the user is able to login with valid crededentials', async () => {
+test ('verify the user is able to go to playwright website', async ({page}) => {
 
-await page.goto (' await page.goto(https://playwright.dev/');
-await expect(page).toHaveTitle('/Playwright/');
+await page.goto ('https://playwright.dev/');
+await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
 
+})
+
+test ('verify the user is unable to login with valid email', async ({page}) => {
+
+await page.goto ('https://www.saucedemo.com/');
+await page.locator('#user-name').fill ('standard_user');
+await page.locator ('#password').fill ('secret_sauce');
+await page.locator('#login-button').click();
+await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 })
